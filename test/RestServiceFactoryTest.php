4b157a88d8d59cfe1491ee5d755a955d98189b52
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2014-08-18 15:25:37 +0200

    Corrected specification of Exception classes

--- test/RestServiceFactoryTest.php
+++ test/RestServiceFactoryTest.php
@@ -95,8 +95,8 @@ class RestServiceFactoryTest extends \PHPUnit_Framework_TestCase
     {
         return array(
             array('\Exception', '500 Server error'),
-            array('\justso\DenyException', '403 Forbidden'),
-            array('\justso\InvalidParameterException', '400 Bad Request'),
+            array('\justso\justso\DenyException', '403 Forbidden'),
+            array('\justso\justso\InvalidParameterException', '400 Bad Request'),
         );
     }
 
