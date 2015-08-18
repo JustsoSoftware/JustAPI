19f4c84632ad68c2c99e655a6e7a186a380099e7
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2015-08-18 16:20:27 +0200

    Moved NotFoundException from JustGen to JustAPI package

---
+++ NotFoundException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Definition of class NotFoundException
+ *
+ * @copyright  2015-today Justso GmbH
+ * @author     j.schirrmacher@justso.de
+ */
+
+namespace justso\justapi;
+
+/**
+ * Class NotFoundException
+ */
+class NotFoundException extends \Exception
+{
+}
